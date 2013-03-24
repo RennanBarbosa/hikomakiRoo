4f58844acb5bfd6fbd7f3942bdb118ce3760e1b5
Rennan Barbosa <rennan.barbosa@dce.ufpb.br>
2013-03-24 20:08:17 -0300

    tentativa

--- WebContent/fuScript.js
+++ WebContent/fuScript.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$("#butget").click(function() {
 		$.getJSON("http://localhost:8080/hikomki-gami/funcionarios", function(data) {
 			for (var i = data.length - 1; i >= 0; i--) {
-				$("#tabela > tbody").append("<tr><td>"+ data[i].nome + "</td>" + "<td>"+ data[i].telefone + "</td>" + "<td>"+ data[i].turno + "</td></tr>");
+				$("#tabela > tbody").append("<tr><td id='nome'>"+ data[i].nome + "</td>" + "<td id='tel'>"+ data[i].telefone + "</td>" + "<td id='turno'>"+ data[i].turno + "</td></tr>");
 			};
 		});
 	});
@@ -21,4 +21,36 @@ $(document).ready(function() {
 		$("#tabela > tbody").html("");
 		alert($("#nome").val()+ " adicionado com sucesso! Use o get e o verá na tabela.");
 	});
+
+/*	$("#butput").click(function(){
+		var j = {"dataNasc":$("#data").val(), "nome":$("#nome").val(), "telefone":$("#tel").val(), "turno":$("#turno").val()};
+
+		$.ajax ({
+			type: "PUT",
+    		url: "http://localhost:8080/hikomki-gami/funcionarios",
+    		data: JSON.stringify(j),
+    		processData: true,
+    		contentType: "application/json"
+		});
+
+		$("#tabela > tbody").html("");
+		alert($("#nome").val()+ " alterado com sucesso! Use o get e o verá na tabela.");
+	});
+
+	$("#butdel").click(function(){
+		var j = {"dataNasc":$("#data").val(), "nome":$("#nome").val(), "telefone":$("#tel").val(), "turno":$("#turno").val()};
+
+		$.ajax ({
+			type: "DELETE",
+    		url: "http://localhost:8080/hikomki-gami/funcionarios",
+    		data: JSON.stringify(j),
+    		processData: true,
+    		contentType: "application/json"
+		});
+
+		$("#tabela > tbody").html("");
+		alert($("#nome").val()+ " excluído com sucesso! Use o get e o verá na tabela.");
+	});
+*/
+
 });
