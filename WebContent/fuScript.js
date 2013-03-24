e406e7505d580fca869be51af1ee511f583be4f8
Rennan Barbosa <rennan.barbosa@dce.ufpb.br>
2013-03-24 16:22:19 -0300

    post de funcionario

--- WebContent/fuScript.js
+++ WebContent/fuScript.js
@@ -6,23 +6,23 @@ $(document).ready(function() {
 			};
 		});
 	});
-});	
 
-$(document).ready(function(){
 	$("#butpost").click(function(){
-		var j = {"dataNasc":$("#data").val(), "nome":$("#nome").val(), "telefone":$("#tel").val(), "turno":$("#turno").val()}
+		var j = {"dataNasc":$("#data").val(), "nome":$("#nome").val(), "telefone":$("#tel").val(), "turno":$("#turno").val()};
+		//var j = {"dataNasc": "10/10/1010", "nome":"luluzinha", "telefone": "88888888", "turno":"night"};
+		
+		//alert(JSON.stringify(j));
 
 		$.ajax ({
 			type: "POST",
     		url: "http://localhost:8080/hikomki-gami/funcionarios",
     		data: JSON.stringify(j),
     		processData: true,
-    		contentType: "application/json",
-    		dataType: 'jsonp',
-    		headers: {
-    			Accept: "application/json"
-    		}
+    		contentType: "application/json"
+		}).done(function(res) { 
+			alert('done') 
 		});
+		
 		//$.post("http://localhost:8080/hikomki-gami/funcionarios", {"dataNasc":25/03/1666, "nome":HellBitch, "telefone":(83)00001000, "turno":Manhã});
 
 	});
