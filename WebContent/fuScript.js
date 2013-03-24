4f91b16d7a49829b495e5625cef75e446abf96a9
Rennan Barbosa <rennan.barbosa@dce.ufpb.br>
2013-03-24 16:49:04 -0300

    post funcionario

--- WebContent/fuScript.js
+++ WebContent/fuScript.js
@@ -2,16 +2,13 @@ $(document).ready(function() {
 	$("#butget").click(function() {
 		$.getJSON("http://localhost:8080/hikomki-gami/funcionarios", function(data) {
 			for (var i = data.length - 1; i >= 0; i--) {
-				$("#tabela").append("<tr><td>"+ data[i].nome + "</td>" + "<td>"+ data[i].telefone + "</td>" + "<td>"+ data[i].turno + "</td></tr>");
+				$("#tabela > tbody").append("<tr><td>"+ data[i].nome + "</td>" + "<td>"+ data[i].telefone + "</td>" + "<td>"+ data[i].turno + "</td></tr>");
 			};
 		});
 	});
 
 	$("#butpost").click(function(){
 		var j = {"dataNasc":$("#data").val(), "nome":$("#nome").val(), "telefone":$("#tel").val(), "turno":$("#turno").val()};
-		//var j = {"dataNasc": "10/10/1010", "nome":"luluzinha", "telefone": "88888888", "turno":"night"};
-		
-		//alert(JSON.stringify(j));
 
 		$.ajax ({
 			type: "POST",
@@ -19,11 +16,9 @@ $(document).ready(function() {
     		data: JSON.stringify(j),
     		processData: true,
     		contentType: "application/json"
-		}).done(function(res) { 
-			alert('done') 
 		});
-		
-		//$.post("http://localhost:8080/hikomki-gami/funcionarios", {"dataNasc":25/03/1666, "nome":HellBitch, "telefone":(83)00001000, "turno":Manhã});
 
+		$("#tabela > tbody").html("");
+		alert($("#nome").val()+ " adicionado com sucesso! Use o get e o verá na tabela.");
 	});
 });
