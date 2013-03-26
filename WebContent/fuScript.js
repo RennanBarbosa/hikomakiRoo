df4b5a2ab325b4335aaa8db79541d9bda71bba10
Rennan Barbosa <rennan.barbosa@dce.ufpb.br>
2013-03-25 21:25:16 -0300

    delete OK e put not OK para funcionario

--- WebContent/fuScript.js
+++ WebContent/fuScript.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$("#butget").click(function() {
 		$.getJSON("http://localhost:8080/hikomki-gami/funcionarios", function(data) {
 			for (var i = data.length - 1; i >= 0; i--) {
-				$("#tabela > tbody").append("<tr><td id='nome'>"+ data[i].nome + "</td>" + "<td id='tel'>"+ data[i].telefone + "</td>" + "<td id='turno'>"+ data[i].turno + "</td></tr>");
+				$("#tabela > tbody").append("<tr><td id='cod'>"+ data[i].id +"</td><td id='nome'>"+ data[i].nome + "</td>" + "<td id='tel'>"+ data[i].telefone + "</td>" + "<td id='turno'>"+ data[i].turno + "</td></tr>");
 			};
 		});
 	});
@@ -22,12 +22,12 @@ $(document).ready(function() {
 		alert($("#nome").val()+ " adicionado com sucesso! Use o get e o verá na tabela.");
 	});
 
-/*	$("#butput").click(function(){
+	$("#butput").click(function(){
 		var j = {"dataNasc":$("#data").val(), "nome":$("#nome").val(), "telefone":$("#tel").val(), "turno":$("#turno").val()};
 
 		$.ajax ({
 			type: "PUT",
-    		url: "http://localhost:8080/hikomki-gami/funcionarios",
+    		url: "http://localhost:8080/hikomki-gami/funcionarios"+$("#idt").val(),
     		data: JSON.stringify(j),
     		processData: true,
     		contentType: "application/json"
@@ -38,19 +38,16 @@ $(document).ready(function() {
 	});
 
 	$("#butdel").click(function(){
-		var j = {"dataNasc":$("#data").val(), "nome":$("#nome").val(), "telefone":$("#tel").val(), "turno":$("#turno").val()};
-
+		
 		$.ajax ({
 			type: "DELETE",
-    		url: "http://localhost:8080/hikomki-gami/funcionarios",
-    		data: JSON.stringify(j),
+    		url: "http://localhost:8080/hikomki-gami/funcionarios/"+$("#idt").val(),
     		processData: true,
     		contentType: "application/json"
 		});
 
 		$("#tabela > tbody").html("");
-		alert($("#nome").val()+ " excluído com sucesso! Use o get e o verá na tabela.");
+		alert("Excluído com sucesso! Use o get e o verá na tabela.");
+		clean()
 	});
-*/
-
 });
